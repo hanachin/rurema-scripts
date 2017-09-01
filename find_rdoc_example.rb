789b2ff08ff7c6e9b3b659e2bdaedf9e4a420961
Seiei Miyagi <hanachin@gmail.com>
2017-09-01 16:16:07 +0900

    Find the method that documented in rdoc but not documented in rurema

---
+++ find_rdoc_example.rb
@@ -0,0 +1,75 @@
+require 'open3'
+
+built_in_klasses = [
+  BasicObject,
+  Object,
+  # ARGF.class,
+  Array,
+  Binding,
+  # ConditionVariable,
+  # Data,
+  Dir,
+  Encoding,
+  Encoding::Converter,
+  Enumerator,
+  Enumerator::Lazy,
+  FalseClass,
+  Fiber,
+  File::Stat,
+  Hash,
+  IO,
+  File,
+  MatchData,
+  Method,
+  Module,
+  Class,
+  # Mutex,
+  NilClass,
+  Numeric,
+  Complex,
+  Float,
+  Integer,
+  Bignum,
+  Fixnum,
+  Rational,
+  ObjectSpace::WeakMap,
+  Proc,
+  Process::Status,
+  # Queue,
+  Random,
+  Range,
+  Regexp,
+  RubyVM,
+  RubyVM::InstructionSequence,
+  # SizedQueue,
+  String,
+  Struct,
+  Struct::Tms,
+  Symbol,
+  Thread,
+  Thread::Backtrace::Location,
+  Thread::ConditionVariable,
+  Thread::Mutex,
+  Thread::Queue,
+  Thread::SizedQueue,
+  ThreadGroup,
+  Time,
+  TracePoint,
+  TrueClass,
+  UnboundMethod
+]
+
+built_in_klasses.each do |klass|
+  klass.instance_methods(false).each do |method|
+    m = klass.instance_method(method)
+    o, s = Open3.capture2('ri', '--no-pager', '--no-site', '--no-gems', '--no-home', "#{m.owner}##{method}")
+    rdoc =  s.success? && o&.match(/^ +..*$/)
+
+    o, s = Open3.capture2('refe', "#{m.owner}##{method}")
+    rurema =  s.success? && o&.match(/^ +..*$/)
+
+    if rdoc && !rurema
+      puts "#{m.owner}##{method}"
+    end
+  end
+end
